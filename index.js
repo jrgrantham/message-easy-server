96b746b03bbd28fa823ced8d9d50f11e16a3a650
James Grantham <james.r.grantham@gmail.com>
2020-04-09 16:15:11 +0100

    knex configuration

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require('express')
-const cors = require('cors')
-
-const server = express()
-
-server.use(cors())
-server.use(express.json())
-
-
-server.get( '*', handleRequest)
-
-function handleRequest(req, res) {
-  res.json('hello')
-}
+const server = require('./server')
 
 const port = process.env.PORT || 3000
 
@@ -19,6 +6,3 @@ server.listen(port, () => {
   console.log('listening on ' + port);
   
 })
-
-
-console.log('boom!');
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet');
+
+const server = express()
+
+server.use(helmet())
+server.use(cors())
+server.use(express.json())
+
+
+server.get( '*', handleRequest)
+
+function handleRequest(req, res) {
+  res.json('hello')
+}
+
+module.exports = server
